bf254e58815167912ef4ae331a1e9a50982f6ef4
jeffjmei <jeffreymei926@gmail.com>
2025-05-09 09:36:12 -0700

    docs(mod_idx): updated roxygen documentation

--- R/my-functions.R
+++ R/my-functions.R
@@ -1,12 +1,17 @@
-#' mod_idx
+#' Circular Modular Index
 #'
-#' produces equivariant index with circular embedding
+#' Computes a modular (wrap-around) index in the range `1` to `n`, useful for circular embeddings.
 #'
-#' @param i original index
-#' @return modularized index
+#' @param i An integer or vector of integers indicating the original index or indices.
+#' @param n A positive integer indicating the modulus (i.e., total number of positions).
+#'
+#' @return An integer or vector of integers, each in the range `1` to `n`, representing the modularized index.
+#'
 #' @examples
 #' mod_idx(10, 10) # 10
 #' mod_idx(11, 10) # 1
+#' mod_idx(c(0, -1, 21), 10) # 10, 9, 1
+#'
 #' @export
 mod_idx <- function(i, n) {
   (i - 1) %% n + 1
@@ -144,5 +149,9 @@ ece <- function(X, L = 2, normalize = FALSE) {
 }
 
 ece.conf.int <- function() {
+
+}
+
+ece.pval <- function() {
 
 }
