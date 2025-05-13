77560103ce8f3878ce9bca279935dac977fe4a87
jeffjmei <jeffreymei926@gmail.com>
2025-05-13 08:51:10 -0700

    test: type I error is controlled

--- tests/testthat/test-ece_test.R
+++ tests/testthat/test-ece_test.R
@@ -31,3 +31,15 @@ test_that("estimator is unbiased under null", {
   rxy <- params$S[1, 2] / sqrt(params$S[1, 1] * params$S[2, 2])
   expect_true((ci[1] < rxy) & (rxy < ci[2]))
 })
+
+test_that("type I error rate is controlled", {
+  params <- scenario(10, n = 1000, sxy = 0, sx = 2, sy = 3)
+
+  pvals <- map_dbl(1:1000, ~ {
+    X <- generate_data(params)
+    ece.test(X[, 1], X[, 2])$p.value
+  })
+
+  type1_error_rate <- mean(pvals < 0.05)
+  expect_lt(abs(type1_error_rate - 0.05), 0.015)
+})
