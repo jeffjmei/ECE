a1a2f2fc901e9ed40b1167fa94e340cc171b8eee
jeffjmei <jeffreymei926@gmail.com>
2025-05-13 09:00:22 -0700

    feat: use namespace to call MASS::mvrnorm

--- simulations/generate_data.R
+++ simulations/generate_data.R
@@ -1,5 +1,5 @@
 generate_data <- function(params) {
-  e <- mvrnorm(params$n, c(0, 0), params$S)
+  e <- MASS::mvrnorm(params$n, c(0, 0), params$S)
   X <- params$h + e
   colnames(X) <- paste0("x", 1:nrow(params$S))
   return(X)
