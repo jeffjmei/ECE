edbd8cc9f00b42ab332060106e1438abaf8e9ba1
jeffjmei <jeffreymei926@gmail.com>
2025-05-23 09:20:17 -0700

    feat(plot_scenario): change plot title

--- R/simulations.R
+++ R/simulations.R
@@ -245,7 +245,7 @@ plot_scenario <- function(params, main = "Scenario Example") {
     geom_line(aes(y = h1), color = "red", linewidth = 1.5) +
     geom_line(aes(y = h2), color = "blue", linewidth = 1.5) +
     labs(
-      title = glue::glue("Scenario Instance (n = {params$n}, Scenario {params$scenario})"),
+      title = glue::glue("Scenario {params$scenario} (n = {params$n})"),
       x = "Index",
       y = "Value"
     ) +
