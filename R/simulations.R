9dad209254fe8288e259539278c8f8dba83e5809
jeffjmei <jeffreymei926@gmail.com>
2025-06-04 09:32:13 -0700

    refactor: generalize code for calculating metrics

--- R/simulations.R
+++ R/simulations.R
@@ -1,3 +1,107 @@
+demean <- function(X, method, params, ...) {
+  if (method == "ECE") {
+    X_mean <- 0 # do nothing
+  } else if (method == "demean") {
+    X_mean <- segment_mean(X, ...)
+  } else if (method == "desmooth") {
+    X_mean <- smooth_mean(X, method = method, ...)
+  } else if (method == "oracle") {
+    X_mean <- cbind(
+      X1 = params$h[, 1],
+      X2 = params$h[, 2]
+    )
+  } else if (method == "oracle cp") {
+    X_mean <- cbind(
+      X1 = segmented_mean(X[, 1], find_cp(params$h)),
+      X2 = segmented_mean(X[, 2], find_cp(params$h))
+    )
+  }
+  return(X - X_mean)
+}
+
+simulate_metric <- function(method, metric, params, n_sim = 1000, ...) {
+  available_methods <- c(
+    "ECE",
+    "demean",
+    "desmooth",
+    "oracle",
+    "oracle_cp"
+  )
+  if (!(method %in% available_methods)) {
+    stop("No such property. Please choose from power, est, mse")
+  }
+
+  # simulate correlation
+  sim <- map(1:n_sim, ~ {
+    # generate data
+    X <- generate_data(params)
+
+    # apply correlation
+    if (method == "ECE") {
+      cor_obj <- ece.test(X[, 1], X[, 2])
+    } else {
+      X_demean <- demean(X, method, params, ...)
+      cor_obj <- cor.test(X_demean[, 1], X_demean[, 2])
+    }
+  })
+  est <- map_dbl(sim, "estimate")
+  pval <- map_dbl(sim, "p.value")
+
+  # extract value
+  if (metric == "power") {
+    mean(pval < 0.05)
+  } else if (metric == "est") {
+    mean(est)
+  } else if (metric == "mse") {
+    rxy <- params$S[1, 2] / sqrt(params$S[1, 1] * params$S[2, 2])
+    mean((cor_obj$estimate - rxy)^2)
+  } else if (metric == "type1") {
+    list(
+      type1 = mean(pval < 0.05),
+      sd = sd(pval < 0.05)
+    )
+  }
+}
+
+simulate_grid <- function(param_grid, metric, export_file) {
+  progress_ct <- 0 # global counter
+  sim <- pmap_dfr(
+    param_grid,
+    function(scenario, method, sxy, sx, sy, n, signal, n_sim) {
+      # update counter
+      progress_ct <<- progress_ct + 1
+      message("Running: ", progress_ct, "/", nrow(param_grid))
+
+      # pull scenario information
+      params <- scenario(
+        scenario_num = scenario,
+        sxy = sxy,
+        sx = sx,
+        sy = sy,
+        n = n,
+        signal = signal
+      )
+
+      # run simulation
+      export_vals <- simulate_metric(
+        method = method,
+        metric = metric,
+        params = params,
+        n_sim = n_sim
+      )
+
+      # export to csv
+      export_simulations(
+        export_vals,
+        method = method,
+        params = params,
+        n_sims = n_sim,
+        export_file = export_file
+      )
+    }
+  )
+}
+
 simulate_power_demean <- function(params, n_sim = 1000, ...) {
   pval <- replicate(n_sim, {
     X <- generate_data(params) # generate data
@@ -284,61 +388,4 @@ export_simulations <- function(..., method, params, n_sims, export_file) {
     # Append without writing header
     write.table(row, export_file, sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
   }
-}
-
-simulate_value <- function(param_grid, sim_type, export_file) {
-  progress_ct <- 0 # global counter
-  sim_power <- pmap_dfr(
-    param_grid,
-    function(scenario, method, sxy, sx, sy, n, signal, n_sim) {
-      # update counter
-      progress_ct <<- progress_ct + 1
-      message("Running: ", progress_ct, "/", nrow(param_grid))
-
-      # pull scenario information
-      params <- scenario(
-        scenario_num = scenario,
-        sxy = sxy,
-        sx = sx,
-        sy = sy,
-        n = n,
-        signal = signal
-      )
-
-      # run simulation
-      if (sim_type == "power") {
-        val <- simulate_power(params, method = method, n_sim = n_sim)
-        export_vals <- list(val = val)
-      } else if (sim_type == "est") {
-        val <- simulate_est(params, method = method, n_sim = n_sim)
-        export_vals <- list(val = val)
-      } else if (sim_type == "mse") {
-        val <- simulate_mse(params, method = method, n_sim = n_sim)
-        export_vals <- list(val = val)
-      } else {
-        stop("Unknown sim_type: please select from power, est, mse")
-      }
-      export_simulations(
-        export_vals,
-        method = method,
-        params = params,
-        n_sims = n_sim,
-        export_file = export_file
-      )
-      # output
-      row <- data.frame(
-        export_vals,
-        method = method,
-        n = params$n,
-        sx = sqrt(params$S[1, 1]),
-        sy = sqrt(params$S[2, 2]),
-        sxy = params$S[1, 2],
-        signal = params$signal,
-        scenario_num = params$scenario,
-        n_sims = n_sim,
-        datetime = Sys.time()
-      )
-      tibble(row)
-    }
-  )
 }
