6c32546aeb0a25524e6ac2e8a8f4859f0c86969a
jeffjmei <jeffreymei926@gmail.com>
2025-06-04 10:15:06 -0700

    refactor: move plots for simulation to simulations-plots.R

---
+++ R/simulations-plots.R
@@ -0,0 +1,65 @@
+plot_power <- function(sim_power, sample_size, scenario, n_simulations, signal_val = 1) {
+  sim_power %>%
+    filter(
+      n == sample_size,
+      scenario_num == scenario,
+      n_sims == n_simulations,
+      signal == signal_val
+    ) %>%
+    ggplot(aes(x = sxy, y = val, color = method)) +
+    geom_line(linewidth = 1) +
+    geom_point(size = 2) +
+    ylim(0, 1) +
+    geom_hline(yintercept = 0.05) +
+    labs(
+      title = glue::glue("Power (n = {sample_size}, Scenario {scenario})"),
+      x = "True Correlation (sxy)",
+      y = "Power",
+      color = "Method"
+    ) +
+    theme_minimal()
+}
+
+plot_est <- function(sim_est, sample_size, scenario, n_simulations, signal_val = 1) {
+  sim_est %>%
+    filter(
+      n == sample_size,
+      scenario_num == scenario,
+      n_sims == n_simulations,
+      signal == signal_val
+    ) %>%
+    ggplot(aes(x = sxy, y = sxy - val, color = method)) +
+    geom_line(linewidth = 1) +
+    geom_point(size = 2) +
+    geom_hline(yintercept = 0) +
+    # xlim(0, 0.55) +
+    # ylim(0, 0.55) +
+    # geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+    labs(
+      title = glue::glue("Correlation Bias (n = {sample_size}, Scenario {scenario})"),
+      x = "True Correlation (sxy)",
+      y = "Bias",
+      color = "Method"
+    ) +
+    theme_minimal()
+}
+
+plot_mse <- function(sim_est, sample_size, scenario, n_simulations, signal_val = 1) {
+  sim_mse %>%
+    filter(
+      n == sample_size,
+      scenario_num == scenario,
+      n_sims == n_simulations,
+      signal == signal_val
+    ) %>%
+    ggplot(aes(x = sxy, y = val, color = method)) +
+    geom_line(linewidth = 1) +
+    geom_point(size = 2) +
+    labs(
+      title = glue::glue("MSE vs Correlation (n = {sample_size}, Scenario {scenario})"),
+      x = "Correlation (sxy)",
+      y = "MSE",
+      color = "Method"
+    ) +
+    theme_minimal()
+}
--- R/simulations.R
+++ R/simulations.R
@@ -63,6 +63,29 @@ simulate_metric <- function(method, metric, params, n_sim = 1000, ...) {
   }
 }
 
+export_simulations <- function(..., method, params, n_sims, export_file) {
+  row <- data.frame(
+    ...,
+    method = method,
+    n = params$n,
+    sx = sqrt(params$S[1, 1]),
+    sy = sqrt(params$S[2, 2]),
+    sxy = params$S[1, 2],
+    signal = params$signal,
+    scenario_num = params$scenario,
+    n_sims = n_sims,
+    datetime = Sys.time()
+  )
+
+  # If file doesn't exist, write with header
+  if (!file.exists(export_file)) {
+    write.table(row, export_file, sep = ",", row.names = FALSE, col.names = TRUE)
+  } else {
+    # Append without writing header
+    write.table(row, export_file, sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
+  }
+}
+
 simulate_grid <- function(param_grid, metric, export_file) {
   progress_ct <- 0 # global counter
   sim <- pmap_dfr(
@@ -100,93 +123,4 @@ simulate_grid <- function(param_grid, metric, export_file) {
       )
     }
   )
-}
-
-plot_power <- function(sim_power, sample_size, scenario, n_simulations, signal_val = 1) {
-  sim_power %>%
-    filter(
-      n == sample_size,
-      scenario_num == scenario,
-      n_sims == n_simulations,
-      signal == signal_val
-    ) %>%
-    ggplot(aes(x = sxy, y = val, color = method)) +
-    geom_line(linewidth = 1) +
-    geom_point(size = 2) +
-    ylim(0, 1) +
-    geom_hline(yintercept = 0.05) +
-    labs(
-      title = glue::glue("Power (n = {sample_size}, Scenario {scenario})"),
-      x = "True Correlation (sxy)",
-      y = "Power",
-      color = "Method"
-    ) +
-    theme_minimal()
-}
-
-plot_est <- function(sim_est, sample_size, scenario, n_simulations, signal_val = 1) {
-  sim_est %>%
-    filter(
-      n == sample_size,
-      scenario_num == scenario,
-      n_sims == n_simulations,
-      signal == signal_val
-    ) %>%
-    ggplot(aes(x = sxy, y = sxy - val, color = method)) +
-    geom_line(linewidth = 1) +
-    geom_point(size = 2) +
-    geom_hline(yintercept = 0) +
-    # xlim(0, 0.55) +
-    # ylim(0, 0.55) +
-    # geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
-    labs(
-      title = glue::glue("Correlation Bias (n = {sample_size}, Scenario {scenario})"),
-      x = "True Correlation (sxy)",
-      y = "Bias",
-      color = "Method"
-    ) +
-    theme_minimal()
-}
-
-plot_mse <- function(sim_est, sample_size, scenario, n_simulations, signal_val = 1) {
-  sim_mse %>%
-    filter(
-      n == sample_size,
-      scenario_num == scenario,
-      n_sims == n_simulations,
-      signal == signal_val
-    ) %>%
-    ggplot(aes(x = sxy, y = val, color = method)) +
-    geom_line(linewidth = 1) +
-    geom_point(size = 2) +
-    labs(
-      title = glue::glue("MSE vs Correlation (n = {sample_size}, Scenario {scenario})"),
-      x = "Correlation (sxy)",
-      y = "MSE",
-      color = "Method"
-    ) +
-    theme_minimal()
-}
-
-export_simulations <- function(..., method, params, n_sims, export_file) {
-  row <- data.frame(
-    ...,
-    method = method,
-    n = params$n,
-    sx = sqrt(params$S[1, 1]),
-    sy = sqrt(params$S[2, 2]),
-    sxy = params$S[1, 2],
-    signal = params$signal,
-    scenario_num = params$scenario,
-    n_sims = n_sims,
-    datetime = Sys.time()
-  )
-
-  # If file doesn't exist, write with header
-  if (!file.exists(export_file)) {
-    write.table(row, export_file, sep = ",", row.names = FALSE, col.names = TRUE)
-  } else {
-    # Append without writing header
-    write.table(row, export_file, sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
-  }
 }
