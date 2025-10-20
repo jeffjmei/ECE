70083de4ec732e0583d0ce3b511f630d0d06b971
jeffjmei <jeffreymei926@gmail.com>
2025-10-20 08:29:58 -0700

    fix: add edge case for scenario13 (random-walk)

--- R/generate_data.R
+++ R/generate_data.R
@@ -1,14 +1,14 @@
 generate_data <- function(params) {
-  if (params$scenario < 13) {
-    e <- MASS::mvrnorm(params$n, c(0, 0), params$S)
-    X <- params$h + e
-    colnames(X) <- paste0("x", 1:nrow(params$S))
-    return(X)
-  } else if (params$scenario == 13) {
+  if (params$scenario == 13) {
     e <- MASS::mvrnorm(params$n, c(0, 0), params$S)
     ex <- cumsum(e[, 1])
     ey <- cumsum(e[, 2])
     X <- cbind(ex, ey)
+    colnames(X) <- paste0("x", 1:nrow(params$S))
+    return(X)
+  } else {
+    e <- MASS::mvrnorm(params$n, c(0, 0), params$S)
+    X <- params$h + e
     colnames(X) <- paste0("x", 1:nrow(params$S))
     return(X)
   }
