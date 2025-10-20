47d5f8c122daa5d7bf3f723b371f370d94c53ae2
jeffjmei <jeffreymei926@gmail.com>
2025-10-20 08:33:00 -0700

    feat(var_corr): add function to estimate the variance of ece correlation

--- R/theory.R
+++ R/theory.R
@@ -112,3 +112,58 @@ ece.cor.asymp <- function(params) {
   rho_var <- (t(dg(u1, u2, u3, u4, u5, u6)) %*% S_u %*% dg(u1, u2, u3, u4, u5, u6))[1, 1]
   return(rho_var)
 }
+
+var_corr <- function(n, sx, sy, sxy, hx, hy,
+                     k13, k31, k22, k04, k40, w1, w2, w12) {
+  rxy <- 0
+  k22 <- 1 + 2 * rxy^2 # NOTE: assuming normality
+  k40 <- 3 # NOTE: assuming normality
+  k04 <- 3 # NOTE: assuming normality
+  k31 <- 3 * rxy # NOTE: assuming normality
+  k13 <- 3 * rxy # NOTE: assuming normality
+
+  S11 <- var_Tk(n, sx, hx, k40, k = 1)
+  S12 <- cov_Th_Tk(n, sx, hx, k40)
+  S13 <- cov_Tk_Rk(n, sx, sy, sxy, hx, hy, k22, k = 1)
+  S14 <- cov_Th_Rk(n, sx, sy, sxy, hx, hy, k22)
+  S15 <- cov_Tk_Qk(n, sx, sy, sxy, hx, hy, k31, k = 1)
+  S16 <- cov_Th_Qk(n, sx, sy, sxy, hx, hy, k31)
+  S22 <- var_Tk(n, sx, hx, k40, k = 2)
+  S23 <- cov_Th_Rk(n, sx, sy, sxy, hx, hy, k22)
+  S24 <- cov_Tk_Rk(n, sx, sy, sxy, hx, hy, k22, k = 2)
+  S25 <- cov_Th_Qk(n, sx, sy, sxy, hx, hy, k31)
+  S26 <- cov_Tk_Qk(n, sx, sy, sxy, hx, hy, k31, k = 2)
+  S33 <- var_Tk(n, sy, hy, k04, k = 1)
+  S34 <- cov_Th_Tk(n, sy, hy, k04)
+  S35 <- cov_Tk_Qk(n, sy, sx, sxy, hy, hx, k13, k = 1)
+  S36 <- cov_Th_Qk(n, sy, sx, sxy, hy, hx, k13)
+  S44 <- var_Tk(n, sy, hy, k04, k = 2)
+  S45 <- cov_Th_Qk(n, sy, sx, sxy, hy, hx, k13)
+  S46 <- cov_Tk_Qk(n, sy, sx, sxy, hy, hx, k13, k = 2)
+  S55 <- var_Qh(n, sx, sy, sxy, hx, hy, k22, k = 1)
+  S56 <- cov_Qh_Qk(n, sx, sy, sxy, hx, hy, k22)
+  S66 <- var_Qh(n, sx, sy, sxy, hx, hy, k22, k = 2)
+
+  dg_mu <- c(
+    -rxy * sx^(-2),
+    0.5 * rxy * sx^(-2),
+    -rxy * sy^(-2),
+    0.5 * rxy * sy^(-2),
+    2 * sx^(-1) * sy^(-1),
+    -1 * sx^(-1) * sy^(-1)
+  )
+
+  S_u_upper <- (1 / (4 * n^2)) * c(
+    S11,
+    S12, S22,
+    S13, S23, S33,
+    S14, S24, S34, S44,
+    S15, S25, S35, S45, S55,
+    S16, S26, S36, S46, S56, S66
+  )
+  S_u <- matrix(0, nrow = 6, ncol = 6)
+  S_u[upper.tri(S_u, diag = TRUE)] <- S_u_upper
+  S_u <- S_u + t(S_u) - diag(diag(S_u))
+  s_rho <- (t(dg_mu) %*% S_u %*% dg_mu)[1, 1]
+  return(s_rho)
+}
