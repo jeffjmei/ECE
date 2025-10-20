3cbf65734176409adc015a0ec3115d5250634afc
jeffjmei <jeffreymei926@gmail.com>
2025-10-20 08:29:25 -0700

    feat(ece.test): generalize to matrices

--- R/ece_test.R
+++ R/ece_test.R
@@ -1,33 +1,27 @@
 # TODO: add docstring
-ece.test <- function(X, Y, L = 2, alpha = 0.05) {
-  if (length(X) != length(Y)) stop("X and Y must have same length")
+ece.test <- function(X, Y = NULL, L = 2, alpha = 0.05) {
+  X <- as.matrix(X)
+  if (!is.null(Y)) {
+    Y <- as.matrix(Y)
+    if (nrow(X) != nrow(Y)) stop("X and Y must have same number of rows")
+    X <- cbind(X, Y)
+  } else {
+    X <- X
+  }
 
   # extract variables
-  n <- length(X)
+  n <- nrow(X)
+  ece_obj <- equiv.cov(X, return.norm = TRUE)
   z <- qnorm(1 - alpha / 2)
-  ece_obj <- equiv.cov(cbind(X, Y), L = 2, return.norm = TRUE)
   sx <- sqrt(ece_obj$cov[1, 1])
   sy <- sqrt(ece_obj$cov[2, 2])
   sxy <- ece_obj$cov[1, 2]
-  wx <- ece_obj$norm[1, 1]
-  wy <- ece_obj$norm[2, 2]
-  wxy <- ece_obj$norm[1, 2]
 
-  # asymptotic variance (rho=0)
-  k22 <- 1 # TODO: true for gaussian (rho=0)
-  rxy <- 0 # TODO: generalize beyond null hypothesis
-  v <- (1 / n) * (
-    k22 +
-      (5 / 2) +
-      (3 / 2) * rxy^2 +
-      sx^(-2) * wx +
-      sy^(-2) * wy +
-      sx^(-1) * sy^(-1) * 2 * rxy * wxy
-  )
+  v <- var_corr_est(n, ece_obj)
+  se <- sqrt(v)
 
   # return values
-  se <- sqrt(v)
-  rxy <- cov2cor(ece_obj$cov)[1, 2] # point estimate
+  rxy <- sxy / (sx * sy) # point estimate
   pval <- 2 * (1 - pnorm(abs(rxy / se))) # p-value
   ci <- c(rxy - z * se, rxy + z * se) # confidence interval
   return(
