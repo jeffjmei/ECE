f98aadf888e91109cea17fc1ad6c7c62bb0952c3
jeffjmei <jeffreymei926@gmail.com>
2026-04-30 08:27:08 -0700

    feat(circ_shift): add function to create shift matrix

--- R/circ.R
+++ R/circ.R
@@ -15,3 +15,21 @@ circ <- function(a) {
   matrix(a[mod_idx(col(matrix(0, n, n)) - row(matrix(0, n, n)) + 1, n)], n, n)
 }
 
+#' Circulant Shift Matrix
+#'
+#' Constructs the \eqn{n \times n} circulant shift matrix \eqn{C_k}, where
+#' entry \eqn{(i,j)} is 1 if \eqn{j - i \equiv k \pmod{n}} and 0 otherwise.
+#'
+#' @param n A positive integer giving the matrix dimension.
+#' @param k A non-negative integer giving the shift.
+#'
+#' @return An \eqn{n \times n} circulant shift matrix.
+#'
+#' @examples
+#' circ_shift(4, 1)
+#'
+#' @export
+circ_shift <- function(n, k) {
+  e_k <- as.numeric(seq_len(n) - 1 == k %% n)
+  circ(e_k)
+}
