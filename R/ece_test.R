98acc8c23668b13e82733770ac1c661f531a5b30
jeffjmei <jeffreymei926@gmail.com>
2026-07-08 15:01:17 -0700

    feat: add mask_disjoint as an argument to bs_parametric_test

--- R/ece_test.R
+++ R/ece_test.R
@@ -200,15 +200,19 @@ bs_multiplier_test <- function(X, B = 1000) {
 }
 
 # Parametric bootstrap test using a bandwidth-2 HAC variance estimate of the lag terms.
-bs_parametric_test <- function(X, B = 1000, force_psd = FALSE) {
+bs_parametric_test <- function(X, B = 1000, mask_disjoint = FALSE, force_psd = FALSE) {
   # Calculate Variance of ECE Terms
   n <- nrow(X)
+  p <- ncol(X)
   S <- ece_terms(X)
   W <- (1 / n^2) * (
     t(S) %*% S +
       t(rotate(S)) %*% S + t(S) %*% rotate(S) +
       t(rotate(S, 2)) %*% S + t(S) %*% rotate(S, 2)
   )
+  O <- pair_overlap(p)
+  stopifnot(identical(colnames(W), colnames(O)))
+  if (mask_disjoint) W[O == 0] <- 0
   if (force_psd) W <- make_psd(W)
 
   # Boostrap Test
@@ -292,6 +296,10 @@ ece.test <- function(X, type = "z.test", B = 1000, conf.level = 0.95,
     bs_parametric_test(X, B)
   } else if (type == "bs.parametric.psd") {
     bs_parametric_test(X, B, force_psd = TRUE)
+  } else if (type == "bs.parametric.mask") {
+    bs_parametric_test(X, B, mask_disjoint = TRUE)
+  } else if (type == "bs.parametric.mask.psd") {
+    bs_parametric_test(X, B, mask_disjoint = TRUE, force_psd = TRUE)
   } else if (type == "bs.parametric.naive") {
     bs_parametric_naive_test(X, B)
   }
